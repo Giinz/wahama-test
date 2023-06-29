7f5ae9b10fc08c57d1a50a77bbbadfe6140c4003
Nguyen Cong Duc <duc1704@gmail.com>
2023-06-29 09:53:56 +0700

    add nprogress

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,7 +4,50 @@ import { ErrorPage, PrivateRoute, UnAuthorized } from '../components/common';
 import Dashboard from '../features/dashboard/pages/Dashboard';
 import ProductList from '../features/dashboard/pages/components/ProductList';
 import CreatePage from '../features/createProduct/pages/CreatePage';
+import asyncComponent from '../utils/asyncFunc';
 
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: '/login',
+//         element: (
+//           <UnAuthorized>
+//             <LoginPage />
+//           </UnAuthorized>
+//         )
+//       },
+//       {
+//         path: '/',
+//         element: (
+//           <UnAuthorized>
+//             <LoginPage />
+//           </UnAuthorized>
+//         )
+//       },
+//       {
+//         path: '/dashboard',
+//         element: (
+//           <PrivateRoute>
+//             <Dashboard />
+//           </PrivateRoute>
+//         ),
+//         children: [
+//           {
+//             element: <ProductList />,
+//             path: 'productList'
+//           },
+//           {
+//             element: <CreatePage />,
+//             path: 'createProduct'
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// ]);
 const router = createBrowserRouter([
   {
     path: '/',
@@ -12,27 +55,19 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/login',
-        element: (
-          <UnAuthorized>
-            <LoginPage />
-          </UnAuthorized>
-        )
+        Component: asyncComponent(() => import('../features/auth/pages/LoginPage'))
       },
-      {
-        path: '/',
-        element: (
-          <UnAuthorized>
-            <LoginPage />
-          </UnAuthorized>
-        )
-      },
+      // {
+      //   path: '/',
+      //   element: (
+      //     <UnAuthorized>
+      //       <LoginPage />
+      //     </UnAuthorized>
+      //   )
+      // },
       {
         path: '/dashboard',
-        element: (
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        ),
+        Component: asyncComponent(() => import('../features/dashboard/pages/Dashboard')),
         children: [
           {
             element: <ProductList />,
@@ -47,5 +82,4 @@ const router = createBrowserRouter([
     ]
   }
 ]);
-
 export default router;
